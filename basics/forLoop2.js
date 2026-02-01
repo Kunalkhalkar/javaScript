ec1bfb6fef6ed90b8924d220acd46f0c4e1b69d3
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-02-01 08:16:56 +0530

    basic for js

---
+++ basics/forLoop2.js
@@ -0,0 +1,5 @@
+/*Q1. Write a java program to print all natural numbers from 1 to n. using for loop*/
+let n = 10;//prompt("enter the value of n");
+for(let  i =1; i<=n ; i++){
+    console.log(i +" ")
+}
---
+++ basics/forLoop3.js
@@ -0,0 +1,6 @@
+/*Q2. Write a java program to print all natural numbers in reverse (from n to 1). using a for loop.
+*/
+let n =10;
+for(let i = n; i>0; i--){
+    console.log(i);
+}
