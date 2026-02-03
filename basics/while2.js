de751101c58fb7576933e0b7bef5157d93c52b62
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-02-03 20:01:59 +0530

    while

---
+++ basics/while2.js
@@ -0,0 +1,9 @@
+/* Q51. Write a js program to display following series :
+           	1  2  2  4  3  6  4  8  5  10  6  12
+*/
+let i = 1, j=2;
+while(i<=6){
+    console.log(i, " "+ j, " ")
+    i++
+    j+=2
+}
