e649b25beda0b1a7923cb1c751252b29fa20cf4f
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-02-20 00:43:52 +0530

    template literals

---
+++ basics/string.js
@@ -0,0 +1,16 @@
+/*
+strings in js
+*/
+let a = 'this is an example of string.'
+console.log(a+"\nthis is a string added at time of printing.");
+
+let boy1 = "swapnil"
+let boy2 = "sopan"
+
+//template literals
+console.log(`${boy1}  is a friend of   ${boy2}`); // Syntax       ` ${boy1}` this are backtic and under that ${ string reference} used 
+
+
+let sentence = `${boy1} ${boy2} are my friends`
+console.log(sentence)
+
