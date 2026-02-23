76338caf4a848898c0ff9f2ea8b89b2dddeb7d2f
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-02-24 00:23:32 +0530

    array method

---
+++ basics/arrays in js/array5.js
@@ -0,0 +1,11 @@
+/*
+array pop() method 
+this method actually works on array i removes the one element from array (last index element) and returns the popped element
+*/
+let a = [10, 20, 30, 40, 50]
+
+
+console.log(a)
+let last = a.pop();
+console.log(last)
+console.log(a)
